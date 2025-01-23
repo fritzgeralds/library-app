1acda9cc730b73dd6ef40ba3f986aad863f826ba
Fritz Dischner <fritz@dischner.me>
2025-01-23 08:52:51 -0800

    add production api endpoint

--- lib/config.ts
+++ lib/config.ts
@@ -1,10 +1,11 @@
 const config = {
   env: {
-    apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT,
+    apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+    prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     imagekit: {
-      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
-      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
-      privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
     },
     databaseUrl: process.env.DATABASE_URL!,
     upstash: {
@@ -15,7 +16,5 @@ const config = {
     },
   },
 };
-
-export const { apiEndpoint, databaseUrl, imagekit, upstash } = config.env;
 
 export default config;
